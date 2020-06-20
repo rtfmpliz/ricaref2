59b5ff2e8af32114e79a5dfd3043de59ea8b8b73
wahyu <wahyu@istw.co.id>
2020-06-20 16:09:06 +0700

    tanbah 2 file

---
+++ Langkah2 pada FormTransaksi.cs
@@ -0,0 +1,26 @@
+langkah2 pada file FormTransaksi.cs
+
+1. panggil form dialogpelanggan
+2. panggil form DialogBarang
+3. buat libary sql
+4. tambahkan variable untuk perintah sql 
+5. import kelas koneksi dari folder (kelas/koneksi.cs) menjadi objek konn
+6.  membuat procedure bersih
+7.  membuat procedure awal
+8.  membuat procedure auto_number
+9.  memanggil prosedure awal, auto_number dan bersih ketika saat pertama kali dijalankan
+10. memanggil procedure awal dan auto_number ketika dijalankan 
+11. membuat procedure simpan_MasterPenjualan
+12. membuat procedure simpan_DetailPenjualan
+13. membuat procedure refresh_penjualan
+14. membuat procedure RefreshTransaksi
+15. Simpan data penjualan perbarang(buat trigger dahulu di sql server untuk automatis stok)
+16. membuat procedure tampilkan data penjualan barang 
+17. hapus data penjualan perbarang
+18. membuat procedure refresh totalseluruh 
+19. memanggil procedure totalseluruh() pada btn_plus click jika data penjualan sudah ada
+20. memanggil procedure totalseluruh() pada btn_plus click jika data penjualan belum ada
+21. memanggil procedure totalseluruh pada btn_minus
+
+Jakarta85
+
